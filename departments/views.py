4d8e82f91813aa36f22698f02a8334a4005e011f
dellsystem <ilostwaldo@gmail.com>
2011-06-09 17:34:27 -0400

    add stuff to models, show course listing in department view

--- departments/views.py
+++ departments/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from departments.models import Department
+from courses.models import Course
 
 def department(request, department):
-	text = "You are trying to view department information for the %s department" % department
+	# Gets passed the short name (4-char), which is the primary key
+	department_name = Department.objects.get(pk=department).get_long_name()
+	text = "The %s department has a long name of %s" % (department, department_name)
+	
+	text += "<br />"
+	
+	# Now show a list of courses in this department
+	courses = Course.objects.filter(department=department)
+	for course in courses:
+		text += "In this dept we have this course: %s<br />" % course
 	return HttpResponse(text)
