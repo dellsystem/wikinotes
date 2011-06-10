9d8ade7d4f9baa3c05fdca5ecde9339a2592b796
dellsystem <ilostwaldo@gmail.com>
2011-06-09 23:26:41 -0400

    add 404 page

--- users/views.py
+++ users/views.py
@@ -5,5 +5,5 @@ from users.models import User
 
 def user(request, username):
 	this_user = get_object_or_404(RealUser, username=username)
-	text = "You are trying to view user information for the user of username %s" % this_user
+	text = "You are trying to view user information for the user of username %s, whose email is %s" % (this_user, this_user.email)
 	return HttpResponse(text)
