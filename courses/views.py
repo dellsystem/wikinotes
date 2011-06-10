d9f8d83a0c50e3ed40af4442664103a16dad70fc
dellsystem <ilostwaldo@gmail.com>
2011-06-10 00:27:35 -0400

    show semesters on course page

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from courses.models import Course
+from semesters.models import CourseSemester
 from semesters.utils import get_current_semester
 from courses.utils import get_current_prof, get_num_watchers
 
@@ -13,5 +14,8 @@ def course(request, department, number):
 	# Get the current semester, and figure out the prof who is teaching this semester
 	current_prof = get_current_prof(this_course, get_current_semester())
 	num_watchers = get_num_watchers(this_course)
+	
+	# Get all the semesters associated with this course (all the CourseSemesters)
+	course_semesters = CourseSemester.objects.filter(course=this_course)
 	
 	return render_to_response('course/overview.html', locals())
