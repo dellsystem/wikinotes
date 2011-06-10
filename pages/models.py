d9ae88b8638560a67e6c28cfdbf09833d4fe484d
dellsystem <ilostwaldo@gmail.com>
2011-06-10 00:21:31 -0400

    improve page types

--- pages/models.py
+++ pages/models.py
@@ -10,23 +10,49 @@ class Page(models.Model):
 
 # Oh fuck this is so awesome
 class LectureNote(Page):
-	num_sections = Model.IntegerField()
 	semester = Model.ForeignKey(Semester)
+	lecture_num = Model.IntegerField()
+	subject = Model.CharField(max_length=100)
+	date = Model.DateField()
+	
+	# Stuff that will need to be displayed for all pages etc
+	def __unicode__(self):
+		return "Lecture %d (%s)" % (self.lecture_num, self.subject)
 	
 class PastExam(Page):
-	num_sections = Model.IntegerField()
-	# Doesn't have a semester
+	# Exam types: final, midterm and that's all for now lol
+	exam_type = Model.CharField(choices=get_possible_exams())
+	# Doesn't have an associated semester
+	# See if it's possible to move semester = ForeignKey into Page and just override it here
 	semester = None
-	# The exam semester ... which means the oldest_year must be pushed back
+	# The exam semester (when the exam was written etc) ... which means the oldest_year must be pushed back
 	exam_semester = Model.ForeignKey(Semester)
+	
+	# Display as: Winter 2008 final exam or Fall 2009 midterm exam
+	def __unicode__(self):
+		return "%s %s exam" % (self.exam_semester, self.exam_type)
 
 class CourseQuiz(Page):
 	semester = Model.ForeignKey(Semester)
+	subject = Model.CharField(max_length=100)
+	
+	def __unicode__(self):
+		return "%s (course quiz)" % self.subject
 
+# The difference between course and vocab quizzes:
+# For course quizzes, you have a question, and several MC answers (like my lab practice final)
+# For vocab quizzes, you just match shit (like terms, definitions etc)
 class VocabQuiz(Page):
 	semester = Model.ForeignKey(Semester)
+	subject = Model.CharField(max_length=100)
+	
+	def __unicode__(self):
+		return "%s (vocabulary quiz)" % self.subject
 
 # Includes miscellaneous shit too
 class CourseSummary(Page):
-	num_sections = Model.IntegerField()
 	semester = Model.ForeignKey(Semester)
+	subject = Model.CharField(max_length=100)
+	
+	def __unicode__(self):
+		return "%s (course summary)" % self.subject
