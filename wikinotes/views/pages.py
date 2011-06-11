4266c3f6f28b2925e933465956d48fd7527eb160
dellsystem <ilostwaldo@gmail.com>
2011-06-11 02:26:43 -0400

    create empty template files for page types

--- wikinotes/views/pages.py
+++ wikinotes/views/pages.py
@@ -1,8 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, Http404
+from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.models import User as RealUser
 from wikinotes.models.pages import Page
+from wikinotes.models.courses import Course
+from django.template import TemplateDoesNotExist
 
 def create(request, department, number, page_type):
 	text = "lol"
-	return HttpResponse(text)
+	this_course = get_object_or_404(Course, department=department, number=int(number))
+	
+	# If the template specified by the page type exists, then we're good to go
+	# Else, 404
+	try:
+		return render_to_response('page/create-%s.html' % page_type, locals())
+	except TemplateDoesNotExist:
+		raise Http404
