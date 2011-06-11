1248961c1ccea4ea8ed2ee037037dfaf28c68014
dellsystem <ilostwaldo@gmail.com>
2011-06-10 21:14:58 -0400

    upgrade to 1.3, create empty wikinotes super-app

--- urls.py
+++ urls.py
@@ -1,26 +1,17 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^wikinotes/', include('wikinotes.foo.urls')),
+    # Examples:
+    # url(r'^$', 'development.views.home', name='home'),
+    # url(r'^development/', include('development.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-    
-    # Viewing a specific user
-    (r'^user/(?P<username>\w+)$', 'users.views.user'),
-    
-    # Viewing a specific course
-    (r'^(?P<department>\w+)_(?P<number>\d+)$', 'courses.views.course'),
-    
-    # Viewing a specific department
-    (r'^(?P<department>\w+)$', 'departments.views.department'),
-    
+    url(r'^admin/', include(admin.site.urls)),
 )
