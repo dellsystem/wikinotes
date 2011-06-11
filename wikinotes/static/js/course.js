d5912b5491c3592e64cab4dfbdb39b9f2bdbe35f
dellsystem <ilostwaldo@gmail.com>
2011-06-11 00:12:09 -0400

    move static files from apache to django

---
+++ wikinotes/static/js/course.js
@@ -0,0 +1,10 @@
+// Some jQuery for controlling the add a page and watch this course buttons
+$(document).ready(function() {
+	$('#watch-button').click(function() {
+		alert("send off a JSON request");
+	});
+	
+	$('#add-button').click(function() {
+		alert("show me the types of pages available and go to whichever template i choose");
+	});
+});
