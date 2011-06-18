e7172e97ec8ff2533aa96cfee243de3d63338411
dellsystem <ilostwaldo@gmail.com>
2011-06-17 22:21:09 -0400

    add a fake faculty image

--- wikinotes/views/courses.py
+++ wikinotes/views/courses.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.utils import simplejson
 from wikinotes.models.courses import Course, CourseSemester
+from wikinotes.models.departments import Department
 from wikinotes.utils.semesters import get_current_semester
 from wikinotes.utils.courses import get_current_profs, get_num_watchers, get_num_pages, is_already_watching
 
@@ -9,7 +10,10 @@ def overview(request, department, number):
 	# If it's a post request, do something
 
 	this_course = get_object_or_404(Course, department=department, number=int(number))
-		
+
+	faculty = Department.objects.get(pk=department).faculty
+	faculty_slug = faculty.slug
+	
 	description = this_course.get_description()
 	course_name = this_course.get_name()
 	credits = this_course.get_credits()
