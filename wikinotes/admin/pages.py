73c36ecde78e96b667d17a1a212c0b910ed21540
dellsystem <ilostwaldo@gmail.com>
2011-06-20 01:25:29 -0400

    add page model to the admin panel, useful for development

--- wikinotes/admin/pages.py
+++ wikinotes/admin/pages.py
@@ -1,7 +1,9 @@
-from wikinotes.models.pages import PageType
+from wikinotes.models.pages import PageType, Page
 from django.contrib import admin
 
 class PageTypeAdmin(admin.ModelAdmin):
 	list_display = ('name', 'slug', 'to_str', 'need_date')
 
 admin.site.register(PageType, PageTypeAdmin)
+
+admin.site.register(Page)
