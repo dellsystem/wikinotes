cc91f4811213606b54f9df7fe95cf2f63b313a4e
dellsystem <ilostwaldo@gmail.com>
2011-06-26 19:11:00 -0400

    show list of courses on index page for now

--- wikinotes/views/other.py
+++ wikinotes/views/other.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from wikinotes.models.courses import Course
 
 def index(request):
+	# For now, list all the classes we have on the index page
+	courses = Course.objects.all()
 	if request.user.is_authenticated():
 		return render_to_response("dashboard.html", locals())
 	else:
