61bd749620454f06ea30de5138a8dd6462931c80
dellsystem <ilostwaldo@gmail.com>
2011-06-26 00:18:03 -0400

    make the trailing slash after the "admin" uri optional

--- urls.py
+++ urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/*', include(admin.site.urls)),
     url(r'', include('source.wikinotes.urls')),
 )
