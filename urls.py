4d2596910c1e3bf54e22f53f35fd08a8ecc396fc
Clarence Leung <cleung0@gmail.com>
2011-07-20 10:36:07 -0400

    allowing for differently named django projects

--- urls.py
+++ urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/*', include(admin.site.urls)),
-    url(r'', include('source.wikinotes.urls')),
+    url(r'', include('wikinotes.urls')),
 )
