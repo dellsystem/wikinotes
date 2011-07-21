185054bf80b1d56e684cd66c44f4c88de6345e2c
dellsystem <ilostwaldo@gmail.com>
2011-07-21 00:25:15 -0400

    fix UserProfile.DoesNotExist error (temp fix)
    Theoretically every user should have a UserProfile created upon
    signup. Also, changing from spaces to tabs just because it's easier
    that way when not using vim.

--- wikinotes/views/other.py
+++ wikinotes/views/other.py
@@ -3,12 +3,16 @@ from wikinotes.models.courses import Course
 from wikinotes.models.users import UserProfile
 
 def index(request):
-    this_user = request.user
+	this_user = request.user
 	# For now, list all the classes we have on the index page
-    courses = Course.objects.all()
-    if this_user.is_authenticated():
-        # Get the list of courses the user is watching
-        watchers = UserProfile.objects.get(user=this_user).courses
-        return render_to_response("dashboard.html", locals())
-    else:
-        return render_to_response("index.html", locals())
+	courses = Course.objects.all()
+	if this_user.is_authenticated():
+		# Get the list of courses the user is watching
+		try:
+			watchers = UserProfile.objects.get(user=this_user).courses
+		except UserProfile.DoesNotExist:
+			watchers = None
+
+		return render_to_response("dashboard.html", locals())
+	else:
+		return render_to_response("index.html", locals())
