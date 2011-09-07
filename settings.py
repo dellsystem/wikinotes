9caf20ca147ea68cd17ad7156467c6fcf60ee514
dellsystem <ilostwaldo@gmail.com>
2011-09-06 22:24:24 -0400

    add base template and static files

--- settings.py
+++ settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/waldo/Projects/wikinotes/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,6 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/home/waldo/Projects/wikinotes/assets/',
 )
 
 # List of finder classes that know how to find static files in
@@ -103,6 +104,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'wikinotes.urls'
 
 TEMPLATE_DIRS = (
+    "/home/waldo/Projects/wikinotes/main/templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -116,7 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
