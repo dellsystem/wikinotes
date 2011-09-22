ce71e3246aff715d21122786de9e01213c7561e9
dellsystem <ilostwaldo@gmail.com>
2011-09-22 02:30:57 -0400

    move create-page view from courses.py to pages.py
    Not a big change, it should have been there from the beginning. Also
    added in the necessary lists/etc to the data dictionary so that the
    term and year template variables can be accessed.

--- urls.py
+++ urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^courses/all$', 'wikinotes.views.courses.all'),
     # This needs to be improved
     url(r'^(?P<department>\w{4})_(?P<number>\d{3})$', 'wikinotes.views.courses.overview'),
-    url(r'^(?P<department>\w{4})_(?P<number>\d{3})/create/(?P<page_type>[^/]+)/?$', 'wikinotes.views.courses.create'),
+    url(r'^(?P<department>\w{4})_(?P<number>\d{3})/create/(?P<page_type>[^/]+)/?$', 'wikinotes.views.pages.create'),
     url(r'^(?P<department>\w{4})_(?P<number>\d{3})/(?P<page_type>[^/]+)/(?P<term>\w{4,6})-(?P<year>\d{4})/(?P<slug>[^/]+)/?$', 'wikinotes.views.pages.show'),
     url(r'^(?P<department>\w{4})_(?P<number>\d{3})/(?P<page_type>[^/]+)/(?P<term>\w{4,6})-(?P<year>\d{4})/(?P<slug>[^/]+)/edit/?$', 'wikinotes.views.pages.edit'),
     # url(r'^wikinotes/', include('wikinotes.foo.urls')),
