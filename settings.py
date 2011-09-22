c5b76258dc793099aaf6f52589cbe5b75b949d56
dellsystem <ilostwaldo@gmail.com>
2011-09-21 23:36:22 -0400

    replace references to wikinotes directory with variable in settings.py
    To make it easier to set up for testing. As per the recommendation of
    @StephenHamilton.

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 # Django settings for wikinotes project.
 
+wikinotes_dir = '/home/waldo/Projects/wikinotes/' # change this if you want to run it on your own machine
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -12,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/waldo/Projects/wikinotes/db.sqlite', # Or path to database file if using sqlite3.
+        'NAME': wikinotes_dir + 'db.sqlite', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -56,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/waldo/Projects/wikinotes/static/'
+STATIC_ROOT = wikinotes_dir + 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,7 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/waldo/Projects/wikinotes/assets/',
+    wikinotes_dir + 'assets/',
 )
 
 # List of finder classes that know how to find static files in
@@ -104,7 +106,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'wikinotes.urls'
 
 TEMPLATE_DIRS = (
-    "/home/waldo/Projects/wikinotes/templates",
+    wikinotes_dir + '/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
