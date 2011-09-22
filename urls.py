93f275c5c785c267401d7a14dabebf92778a428a
dellsystem <ilostwaldo@gmail.com>
2011-09-22 02:57:14 -0400

    turns out the course number doesn't have to be a number T_T
    Updated urls.py to reflect that. The method could use improving.

--- urls.py
+++ urls.py
@@ -19,12 +19,12 @@ urlpatterns += patterns('views.courses',
     url(r'^courses$', 'index'),
     url(r'^courses/all$', 'all'),
     url(r'^courses/faculty$', 'faculty'), # for browsing by faculty (i.e. lists all the faculties)
-    url(r'^(?P<department>\w{4})_(?P<number>\d{3})$', 'overview'),
+    url(r'^(?P<department>\w{4})_(?P<number>[0-9a-zA-Z]{3,5})$', 'overview'),
 )
 
 # For viewing, editing and creating pages
 urlpatterns += patterns('views.pages',
-	url(r'^(?P<department>\w{4})_(?P<number>\d{3})/create/(?P<page_type>[^/]+)/?$', 'create'),
-	url(r'^(?P<department>\w{4})_(?P<number>\d{3})/(?P<page_type>[^/]+)/(?P<term>\w{4,6})-(?P<year>\d{4})/(?P<slug>[^/]+)/?$', 'show'),
-	url(r'^(?P<department>\w{4})_(?P<number>\d{3})/(?P<page_type>[^/]+)/(?P<term>\w{4,6})-(?P<year>\d{4})/(?P<slug>[^/]+)/edit/?$', 'edit'),
+	url(r'^(?P<department>\w{4})_(?P<number>[0-9a-zA-Z]{3,5})/create/(?P<page_type>[^/]+)/?$', 'create'),
+	url(r'^(?P<department>\w{4})_(?P<number>[0-9a-zA-Z]{3,5})/(?P<page_type>[^/]+)/(?P<term>\w{4,6})-(?P<year>\d{4})/(?P<slug>[^/]+)/?$', 'show'),
+	url(r'^(?P<department>\w{4})_(?P<number>[0-9a-zA-Z]{3,5})/(?P<page_type>[^/]+)/(?P<term>\w{4,6})-(?P<year>\d{4})/(?P<slug>[^/]+)/edit/?$', 'edit'),
 ),
