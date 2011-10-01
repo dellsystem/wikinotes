d7aa5013a534cd9d1fd01f416ec999fd86e32da4
dellsystem <ilostwaldo@gmail.com>
2011-10-01 16:53:14 -0400

    add gravatar app

--- settings.py
+++ settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'wiki',
+    'gravatar',
     'django.contrib.markup', # for markdown
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
