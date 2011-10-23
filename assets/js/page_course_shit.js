6be0b018e931c3280fc5d0d9b7c7ab85908a9dcf
dellsystem <ilostwaldo@gmail.com>
2011-10-22 22:02:43 -0400

    set tabindex dynamically using js
    Feels so terrible and yet, so good

--- assets/js/page_course_shit.js
+++ assets/js/page_course_shit.js
@@ -78,4 +78,13 @@ $(document).ready(function() {
 	// Tables are by default sortable, add the nosort class to prevent that
 	// http://tablesorter.com/docs/example-meta-sort-list.html for initial sorting order
 	$('table').not('.nosort').tablesorter();
+
+	// Dynamically set tabindex, I'm sorry but it really is difficult to do this otherwise
+	var tabindex = 1;
+    $('input, select, textarea').each(function() {
+        if (this.type !== "hidden") {
+            $(this).attr("tabindex", tabindex);
+            tabindex++;
+        }
+    });
 });
