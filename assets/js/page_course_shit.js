46fd4aee330a74145cf0aa48cdfab9455cc2b5fc
dellsystem <ilostwaldo@gmail.com>
2011-12-05 17:46:46 -0500

    Add math typesetting in preview mode

--- assets/js/page_course_shit.js
+++ assets/js/page_course_shit.js
@@ -142,6 +142,7 @@ $(document).ready(function() {
 			url: '/markdown',
 			success: function(data) {
 				$('#content-preview').html(data);
+				MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'content-preview']);
 			},
 		});
 		$('#edit-buttons').hide();
