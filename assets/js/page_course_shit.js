58d25c3dc044396d69fe722971bc63ca812a3863
dellsystem <ilostwaldo@gmail.com>
2011-12-04 22:54:30 -0500

    Delete unused section-related js
    Never have to deal with that again

--- assets/js/page_course_shit.js
+++ assets/js/page_course_shit.js
@@ -55,50 +55,6 @@ $(document).ready(function() {
 			$('#create-page-popup button').remove();
 		});
 	});
-	$('#add-section').click(function(event) {
-		// Find the last visible section
-		// This code needs to be optimised (later)
-		var thisIDNumber;
-		var thisID;
-		$('fieldset[id^=section-]').not(':hidden').each(function() {
-			thisID = $(this).attr('id');
-			thisIDNumber = parseInt(thisID.substring(thisID.indexOf('-')+1));
-			console.log(thisID + ' is NOT HIDDEN LOL');
-		});
-		// Now check if the new section is already present in the document
-		var nextIDNumber = thisIDNumber + 1;
-		var nextID = 'section-' + nextIDNumber;
-		console.log(nextID);
-		if ($('#' + nextID).length > 0) {
-			// Show it
-			$('#' + nextID).show();
-		} else {
-			// Duplicate the last section element, change its ID and contents, then append it
-			console.log("ADD A NEW ONE");
-			console.log(thisID);
-			// MONKEY don't forget to change all the other stuff too (id, name, text between span, etc)
-			$('#' + thisID).after($('#' + thisID).clone().attr('id', nextID));
-		}
-		// Increase the number of sections in the input field
-		$('#num_sections').val('' + (parseInt($('#num_sections').val(), 10) + 1));
-		event.preventDefault;
-		return false;
-	});
-
-	// Now make the Number of sections dropdown control the number of sections
-	var hideExtraSections = function(numSections) {
-		$('fieldset[id^=section-]').each(function() {
-			// Hide all of the divs with an ID number greater than the selected number of sections
-			var thisID = $(this).attr('id');
-			var thisIDNumber = parseInt(thisID.substring(thisID.indexOf('-')+1));
-			if (thisIDNumber > numSections) {
-				$(this).hide();
-			} else {
-				$(this).show();
-			}
-		});
-	};
-	hideExtraSections(1);
 
 	var inFullscreen = false;
 	$('#fullscreen').click(function(event) {
