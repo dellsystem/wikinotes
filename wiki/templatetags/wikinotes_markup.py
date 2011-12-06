8857247c98ec9cf8483e3629454dfccd6bf8ad11
dellsystem <ilostwaldo@gmail.com>
2011-12-06 01:42:34 -0500

    Remove header ID markdown extension
    Does weird things with # (makes it h3 for some reason)

--- wiki/templatetags/wikinotes_markup.py
+++ wiki/templatetags/wikinotes_markup.py
@@ -7,7 +7,7 @@ register = template.Library()
 # NEEDS TESTS
 @register.filter()
 def wikinotes_markdown(value):
-	return markdown(escape(value).replace('&amp;', '&').replace("\\$", "\\\\$"), 'headerid(level=3),tables,mathjax')
+	return markdown(escape(value).replace('&amp;', '&').replace("\\$", "\\\\$"), 'tables,mathjax')
 
 """
 The alternative to the escaping/removing-then-inserting ampersands thing (which must be done as both markdown and the escape function escape them) is to use just markdown with safe as one of the extensions, but that causes html to appear as [HTML_REMOVED] which is just silly. This way is much better.
