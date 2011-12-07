3c45797e11dd1ae9492e5fc71566dc094d04c8c3
dellsystem <ilostwaldo@gmail.com>
2011-12-07 04:49:30 -0500

    Use python-markdown instead of django builtin
    Better escaping, xhtml1 output, more fine-grained control etc
    
    Also adds some extensions.
    
    Closes #79.

--- wiki/templatetags/wikinotes_markup.py
+++ wiki/templatetags/wikinotes_markup.py
@@ -1,14 +1,13 @@
 from django import template
-from django.contrib.markup.templatetags.markup import markdown
 from django.utils.html import escape
+import markdown
 
 register = template.Library()
+
+md = markdown.Markdown(extensions=['def_list', 'tables', 'mathjax', 'toc', 'footnotes'], safe_mode='escape', output_format='xhtml1')
 
 # NEEDS TESTS
 @register.filter()
 def wikinotes_markdown(value):
-	return markdown(escape(value).replace('&amp;', '&').replace("\\$", "\\\\$"), 'def_list,tables,mathjax')
-
-"""
-The alternative to the escaping/removing-then-inserting ampersands thing (which must be done as both markdown and the escape function escape them) is to use just markdown with safe as one of the extensions, but that causes html to appear as [HTML_REMOVED] which is just silly. This way is much better.
-"""
+	# Replace \$ with \\$ so that markdown doesn't do anything else to (in conjunction with mathjax's processEscapes)
+	return md.convert(value.replace("\\$", "\\\\$"))
