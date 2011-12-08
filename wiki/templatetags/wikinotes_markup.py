00595120be3cdae43f07751d073f73ae47bc11de
dellsystem <ilostwaldo@gmail.com>
2011-12-08 07:23:00 -0500

    Fix footnote-clearing issue (closes #88)
    Footnotes weren't properly resetting upon subsequent passes. Easy fix.

--- wiki/templatetags/wikinotes_markup.py
+++ wiki/templatetags/wikinotes_markup.py
@@ -9,5 +9,7 @@ md = markdown.Markdown(extensions=['subscript', 'superscript', 'urlize', 'nl2br'
 # NEEDS TESTS
 @register.filter()
 def wikinotes_markdown(value):
+	# Must reset it to clear the footnotes and maybe other stuff too
+	md.reset()
 	# Replace \$ with \\$ so that markdown doesn't do anything else to (in conjunction with mathjax's processEscapes)
 	return md.convert(value.replace("\\$", "\\\\$"))
