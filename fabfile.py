affdfee42efdf28b0f3bf3685f4b0ad94e79a465
dellsystem <ilostwaldo@gmail.com>
2012-01-18 15:50:00 -0500

    Add deployment stuff to fabfile (in progress)

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,24 @@
-from fabric.api import local
+from fabric.api import *
+
+def beta():
+	env.hosts = ['deploy@beta.wikinotes.ca']
 
 def less():
-    local("lessc -x assets/css/bootstrap.less > assets/css/bootstrap.css")
+	local("lessc -x assets/css/bootstrap.less > assets/css/bootstrap.css")
+
+def kill():
+	run("kill $(ps aux | grep gunicorn | awk '{ print $2 }' | sort -nr | tail -n 1)")
+
+def update():
+	run("cd /srv/beta/wikinotes; git pull")
+
+def deploy():
+	# Kill the old process
+	kill()
+	# Pull it, updating the source
+	update()
+	# Less minification
+	run("cd /srv/beta/wikinotes; fab less")
+	# Don't need to do collectstatic because it is aliased to assets/ T_T
+	# Start running the new one
+	run("cd /srv/beta; source bin/activate; cd wikinotes; nohup gunicorn_django --workers=2 >> log &")
