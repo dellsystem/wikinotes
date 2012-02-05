7d3edef41c48d9b6c5c97cfcc578d050fb9f7ec8
dellsystem <ilostwaldo@gmail.com>
2012-02-05 15:59:10 -0500

    Exclude some fields from Course admin page

--- wiki/admin/courses.py
+++ wiki/admin/courses.py
@@ -1,6 +1,9 @@
 from wiki.models.courses import Course, CourseSemester, Professor
 from django.contrib import admin
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+	exclude = ('num_watchers', 'latest_activity', 'watchers')
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(CourseSemester)
 admin.site.register(Professor)
