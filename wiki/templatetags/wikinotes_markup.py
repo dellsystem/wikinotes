70d05e0f45df2c79bba8c665203b0245207b5b5a
dellsystem <ilostwaldo@gmail.com>
2012-04-13 15:25:16 -0400

    Replace buggy markdown @mention code with sub
    Temporarily in the wikinotes_markup.py file. TEMPORARILY.

--- wiki/templatetags/wikinotes_markup.py
+++ wiki/templatetags/wikinotes_markup.py
@@ -17,11 +17,18 @@ All extensions beginning with wiki_ are modified version of standard extensions.
 mentions converts all @mentions to [@mention](/user/mention) (though in straight HTML), doesn't check if the user exists or not because it's not that smart but otherwise it shouldn't be too problematic
 """
 
-md = markdown.Markdown(extensions=['mentions', 'wiki_footnotes', 'wiki_toc', 'downheader', 'wiki_tables', 'wiki_codehilite', 'wiki_fenced_code', 'wiki_def_list', 'nl2br', 'subscript', 'superscript', 'mathjax', 'urlize', 'wikilinks'], safe_mode='escape', output_format='xhtml1')
+md = markdown.Markdown(extensions=['wiki_footnotes', 'wiki_toc', 'downheader', 'wiki_tables', 'wiki_codehilite', 'wiki_fenced_code', 'wiki_def_list', 'nl2br', 'subscript', 'superscript', 'mathjax', 'urlize', 'wikilinks'], safe_mode='escape', output_format='xhtml1')
+
+MENTION_RE = re.compile('(?<![a-zA-Z0-9])@(?P<username>\w+)')
+
+def convert_mention(m):
+	username = m.group(1)
+	return '[@' + username + '](/user/' + username + ')'
 
 # NEEDS TESTS
 @register.filter()
 def wikinotes_markdown(value):
+	value = MENTION_RE.sub(convert_mention, value)
 	# Must reset it to clear the footnotes and maybe other stuff too
 	md.reset()
 	# Replace \$ with \\$ so that markdown doesn't do anything else to (in conjunction with mathjax's processEscapes)
