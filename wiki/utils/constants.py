a9865b023c1c0af4cc21885094e932f8e83051a8
dellsystem <ilostwaldo@gmail.com>
2012-12-09 18:35:33 -0500

    Add 2005 to the list of years for past exams

--- wiki/utils/constants.py
+++ wiki/utils/constants.py
@@ -1,5 +1,5 @@
 from wiki.utils.currents import current_year
 
 terms = ['winter', 'summer', 'fall']
-years = range(current_year, 2005, -1)
+years = range(current_year, 2004, -1)
 exam_types = ['midterm', 'final']
