1ecb7657040a129da1b2ce4a48b2b2a685d10df5
dellsystem <ilostwaldo@gmail.com>
2013-01-21 13:48:11 -0500

    Improve admin view for listing pages

--- wiki/admin/pages.py
+++ wiki/admin/pages.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from wiki.models.pages import Page
 
 
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'page_type', 'course_sem')
+
+
+admin.site.register(Page, PageAdmin)
